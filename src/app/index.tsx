ffb5f5d3b2a4068f9c209046a4edd240e7e18d5c
marwancoder <marwane.ouarradi@gmail.com>
2026-03-19 00:32:34 +0000

    feat: update dependencies and integrate Tailwind CSS with NativeWind
    - Added babel-preset-expo and nativewind to package.json
    - Included prettier-plugin-tailwindcss and tailwindcss for styling
    - Imported global CSS in the root layout
    - Removed the about page and its layout
    - Updated index page to display a title and an image with Tailwind CSS classes
    - Created tailwind.config.js for Tailwind CSS configuration
    - Updated tsconfig.json to include nativewind-env.d.ts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,16 @@
-import { Link } from "expo-router";
+import { Image } from "expo-image";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Bla bla </Text>
-      <Link href={"/about"}>
-        <Text>About</Text>
-      </Link>
+      <Text className="text-amber-500 bg-black p-3 rounded-md font-bold text-2xl mb-5">
+        MARWAN WARRADI
+      </Text>
+      <Image
+        source={require("../../assets/images/icon.png")}
+        style={{ width: 200, height: 200, borderRadius: 20 }}
+      />
     </View>
   );
 }
@@ -17,6 +20,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-    backgroundColor: "red",
   },
 });
